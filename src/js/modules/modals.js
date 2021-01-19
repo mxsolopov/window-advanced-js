14ea6896631495e8b8bf3d4abbf05f5a53112081
Maksim Solopov <mxsolopov@yandex.ru>
2021-01-20 01:09:24 +0300

    Modals script added

---
+++ src/js/modules/modals.js
@@ -0,0 +1,50 @@
+const modals = () => {
+
+    function bindModal(triggerSelector, modalSelector, closeSelector) {
+
+        const trigger = document.querySelectorAll(triggerSelector),
+        modal = document.querySelector(modalSelector),
+        close = document.querySelector(closeSelector);
+
+        // Показать окно при клике на триггер
+        for (let key of trigger) {
+            key.addEventListener('click', (e) => {
+
+                if (e.target) {
+                    e.preventDefault();
+                }
+    
+                modal.style.display = 'block';
+                document.body.style.overflow = 'hidden';
+            });
+        }
+
+        // Скрыть окно при клике на крестик
+        close.addEventListener('click', () => {
+            modal.style.display = 'none';
+            document.body.style.overflow = '';            
+        });
+
+        // Скрыть окно при клике на область вне формы
+        modal.addEventListener('click', (e) => {
+            if (e.target === modal) {
+                modal.style.display = 'none';
+                document.body.style.overflow = ''; 
+            }
+        });
+    }
+
+    // Показать форму при спустя заданное время
+    function showModalByTime(selector, time) {
+        setTimeout(function() {
+            document.querySelector(selector).style.display = 'block';
+            document.body.style.overflow = "hidden";
+        }, time);
+    }
+
+    bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+    bindModal('.phone_link', '.popup', '.popup .popup_close');
+    showModalByTime('.popup', 60000);
+};
+
+export default modals;
