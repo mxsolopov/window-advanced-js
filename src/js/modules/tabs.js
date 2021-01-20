000d4569f86fafa182176f3bfa2ecc2f014b1052
Maksim Solopov <mxsolopov@yandex.ru>
2021-01-20 15:09:32 +0300

    Main tabs added

---
+++ src/js/modules/tabs.js
@@ -0,0 +1,43 @@
+const tabs = (tabItemBoxSelector, tabItemSelector, tabContentSelector, activeSelector) => {
+
+        const tabItemBox = document.querySelector(tabItemBoxSelector),
+              tabItem = document.querySelectorAll(tabItemSelector),
+              tabContent = document.querySelectorAll(tabContentSelector);
+    
+        // Скрыть контент табов
+        function hideTabContent() {
+            tabContent.forEach(item => {
+                item.style.display = 'none';
+            });
+
+            tabItem.forEach(item => {
+                item.classList.remove(activeSelector);
+            });
+        }
+    
+        // Показать контент активной табы
+        function showTabContent(i = 0) {
+            tabContent[i].style.display = 'block';
+            tabItem[i].classList.add(activeSelector);
+        }
+
+        // Показ контента первой табы
+        hideTabContent();
+        showTabContent();
+
+        // Клик на табу
+        tabItemBox.addEventListener('click', function (e) {
+            let target = e.target;
+            if (target && (target.classList.contains(tabItemSelector.replace(/\./, "")) || 
+            target.parentNode.classList.contains(tabItemSelector.replace(/\./, "")))) {
+                tabItem.forEach( (item, i) =>  {
+                    if (target == item || target.parentNode == item) {
+                        hideTabContent();
+                        showTabContent(i);
+                    }
+                });
+            }
+        });
+};
+
+export default tabs;
