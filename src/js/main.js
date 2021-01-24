dc52f81878a9b6bed236f64aa75e78c79952ce44
Maksim Solopov <mxsolopov@yandex.ru>
2021-01-24 19:11:49 +0300

    Timer added

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,14 @@ import modals from './modules/modals';
 import tabs from './modules/tabs';
 import forms from './modules/forms';
 import changeModalState from './modules/changeModalState';
+import timer from './modules/timer';
 
 window.addEventListener('DOMContentLoaded', () => {
     "use strict";
 
     // Объект для записи данных для расчёта
     let modalState = {};
+    let deadline = '2021-02-12';
 
     changeModalState(modalState);
     modals();
@@ -16,4 +18,5 @@ window.addEventListener('DOMContentLoaded', () => {
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div','after_click');
     tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img','do_image_more', 'inline');
     forms(modalState);
+    timer('.container1', deadline);
 });
