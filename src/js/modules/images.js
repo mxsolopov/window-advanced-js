a1e410dc721a3b2dcd95d84e32804f7bc5e1962a
Maksim Solopov <mxsolopov@yandex.ru>
2021-01-24 19:51:00 +0300

    Image popup added

---
+++ src/js/modules/images.js
@@ -0,0 +1,46 @@
+const images = () => {
+
+    // Создание блока модального изображения
+    const imgPopup = document.createElement('div'),
+        workSection = document.querySelector('.works'),
+        // Создание модального изображения
+        bigImage = document.createElement('img');
+
+    // Стилизация и добавление в вёрстку модального изображения
+    imgPopup.classList.add('popup');
+    workSection.appendChild(imgPopup);
+    imgPopup.appendChild(bigImage);
+    imgPopup.style.cssText = `
+        display: none;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    `;
+    bigImage.style.cssText = `
+        max-height: 90vh;
+    `;
+
+    // Клик по миниатюре изображения
+    workSection.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        let target = e.target;
+
+        // Открытие изображения в полном размере
+        if (target && target.classList.contains('preview')) {
+
+            imgPopup.style.display = 'flex';
+            const path = target.parentNode.getAttribute('href');
+            bigImage.setAttribute('src', path);
+            document.body.style.overflow = 'hidden';
+        }
+
+        // Закрытие изображения при клике на подложку
+        if (target && target.matches('div.popup')) {
+            imgPopup.style.display = 'none';
+            document.body.style.overflow = '';
+        }
+    });
+};
+
+export default images;
